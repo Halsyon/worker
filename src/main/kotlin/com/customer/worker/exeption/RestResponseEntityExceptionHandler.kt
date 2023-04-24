32e3f21e3a916936c215e190c6888d0461799f3c
GrandEmetak <JekHamond@tutanota.com>
2023-04-24 20:22:34 +0500

    feat: created Validate layer info

---
+++ src/main/kotlin/com/customer/worker/exeption/RestResponseEntityExceptionHandler.kt
@@ -0,0 +1,39 @@
+/*
+Copyright [2023] [Halsyon]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+package com.customer.worker.exeption
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.context.request.WebRequest
+import java.time.LocalDateTime
+import javax.servlet.http.HttpServletRequest
+
+@ControllerAdvice
+class RestResponseEntityExceptionHandler {
+
+    @ExceptionHandler()
+    fun handleException(ex: Exception,request: HttpServletRequest): ResponseEntity<ApiError> {
+        var apiError = ApiError(
+            request.requestURI,
+            ex.message.toString(),
+            HttpStatus.BAD_REQUEST.value(),
+            LocalDateTime.now()
+        )
+        return ResponseEntity<ApiError>(apiError, HttpStatus.BAD_REQUEST)
+    }
+}
