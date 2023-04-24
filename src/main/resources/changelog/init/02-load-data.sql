679cd69df7f418dd18a9261c0a95f73f3c4df618
Halsyon <73285357+Halsyon@users.noreply.github.com>
2023-04-24 21:46:17 +0500

    Delete src/main/resources/changelog directory

--- src/main/resources/changelog/init/02-load-data.sql
+++
@@ -1,9 +0,0 @@
--- liquibase formatted sql
-
--- changeset hals:2023-04-22-worker_insert_info
--- comment: предварительное наполнение табл Работник данными
-
-INSERT INTO research."worker".worker (worker_id, name, age, department)
-VALUES (1, 'John Smith', 37, 'IT'),
-       (2, 'Shia LaBeouf', 28, 'IT'),
-       (3, 'Olli Wood', 33, 'IT');
